595f71ecf0360c004457ee55fc1d8d04a98ffe12
DESKTOP-P9DK074\STAR LAPTOPS <umair6029@gmail.com>
2023-12-22 17:08:07 +0500

    is number is power of 4

---
+++ 01_Techie Delight/01_Bit_Manipulation/15_problem_a5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// Approach - 1
+bool isPowerOf4(int n)
+{
+    long long int ans = 1;
+    for (int i = 0; i < 16; i++)
+    {
+        if (ans == n)
+            return 1;
+        ans = ans * 4;
+    }
+    return 0;
+}
+
+// Approach - 2
+bool isPowerOfFour(int n)
+{
+    return !(n & (n - 1)) && (n % 3 == 1);
+}
+int main()
+{
+    long long int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (isPowerOfFour(n))
+    {
+        cout << "Number is power of 4" << endl;
+    }
+    else
+    {
+        cout << "Number is not power of 4" << endl;
+    }
+}
